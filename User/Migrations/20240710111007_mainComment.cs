97aceab4ac05c3b141a500f426e0517a36144842
Sheefoo <n.almalaika@yahoo.com>
2024-07-10 14:34:14 +0200

    fix-bug: resolve wrong vaules of PostId in CommentViewModel

---
+++ User/Migrations/20240710111007_mainComment.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.Migrations
+{
+    /// <inheritdoc />
+    public partial class mainComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "MainComments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MainComments_PostId",
+                table: "MainComments",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MainComments_Posts_PostId",
+                table: "MainComments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MainComments_Posts_PostId",
+                table: "MainComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MainComments_PostId",
+                table: "MainComments");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "MainComments");
+        }
+    }
+}
